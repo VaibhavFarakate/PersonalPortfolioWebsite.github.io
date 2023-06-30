62dd99f5e625feac307a03082bc0db78b383153b
VaibhavFarakate <vaibhav7525@gmail.com>
2023-06-30 17:37:57 +0530

    Projects and other little changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-document.getElementsByClassName("btnToggle").onclick(function () {
-  document.getElementsByClassName("btnToggle").children.style.backgroundColor =
-    "red";
-});
+$(".btnToggle").click(toggler);
+
+function toggler() {
+  var btnId = $(this).data("id");
+
+  var ElementId = "#toggleIcon" + btnId;
+
+  if ($(ElementId).hasClass("down")) {
+    $(ElementId).animate({ rotate: "180deg" }, 300);
+    $(ElementId).addClass("up");
+    $(ElementId).removeClass("down");
+  } else if ($(ElementId).hasClass("up")) {
+    $(ElementId).animate({ rotate: "0deg" }, 300);
+    $(ElementId).addClass("down");
+    $(ElementId).removeClass("up");
+  }
+}
